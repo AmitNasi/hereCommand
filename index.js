4477c0cd83b1cc251ae8d327ce09d88696837d1d
AmitNasi <99030306+AmitNasi@users.noreply.github.com>
2023-10-21 23:27:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,21 @@ const sentences = [
     "Whilst delving into the catacombs and facing the most worrisome of enemies; This is the one player you would want on your side <ign>"
 ];
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
 }
 
 register("command", (name) => {
-    const randomInt = getRandomIntInclusive(1, sentences.length+1)
-    const randomSentence = sentences[randomInt];
+    const newSentences = shuffle(sentences);
+    const randomSentence = newSentences[0];
     const finalSentence = randomSentence.replace("<ign>", name);
 
     executeCommand(`/pc ${finalSentence}`);
