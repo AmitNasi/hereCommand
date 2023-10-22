a757c3a80bc270bf626036e4aeeab041501586aa
AmitNasi <99030306+AmitNasi@users.noreply.github.com>
2023-10-22 10:28:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import welcomeSentences from "./welcomeSentences";
+import artOfWarQuotes from "./artOfWarQuotes";
 
-let idx = 0;
+let welcomeIndex = 0;
+let artOfWarIndex = 0;
 
 register("command", (user) => {
-  const sentence = welcomeSentences[idx];
+  const sentence = welcomeSentences[welcomeIndex];
   const formattedSentence = sentence.replace("<ign>", user);
 
   ChatLib.say("/pc " + formattedSentence);
-  idx++;  
+  welcomeIndex++;  
 }).setName("shalom");
+
+register("command", () => {
+  const sentence = artOfWarQuotes[artOfWarIndex];
+  const formattedSentence = sentence.concat(" - Sun Tzu, The Art of War");
+  
+  ChatLib.say("/pc " + formattedSentence);
+  artOfWarIndex++;  
+}).setName("artofwar");
+
